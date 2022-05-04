c8993dd4238c6684bc473e31f0e37985f21ee218
92summer <80613363+92summer@users.noreply.github.com>
2022-05-05 01:03:01 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,44 @@
+#include "food.h"
+#include "map.h"
+#include "rules.h"
+#include "snake.h"
+#include <conio.h>
+#include <stdio.h>
+#include <stdlib.h>
+//#include <pthread.h>
+
+int main()
+{
+    // system("Command");命令行调用Command命令
+    system("title 贪吃蛇");
+    system("mode con cols=53 lines=23");
+
+    MapInit();
+
+    Snake S = SnakeInit();
+    int direction = right;
+    int endGame_flag = 0;
+    Point FoodPosition = build_food();
+    do
+    {
+        ifEatFood(S, &FoodPosition);
+
+        endGame_flag = if_inWallOrinBody(S);
+
+        if (!_kbhit())
+        {
+            SnakeMove(direction, S, &FoodPosition);
+            Delay_ms(200);//数值越高蛇动越慢
+            continue;
+        }
+        else
+            direction = detect_move(direction);
+
+    } while (!endGame_flag);
+    
+    system("cls");//清屏
+    printf("Game Over!\n");
+    showCursor();
+    system("pause");   
+    return 0;
+}
